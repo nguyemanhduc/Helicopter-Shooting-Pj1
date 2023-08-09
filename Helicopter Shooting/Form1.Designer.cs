fb0783760a6d032dc90d0d8b058f5981296e912c
nguyemanhduc <135008573+nguyemanhduc@users.noreply.github.com>
2023-08-09 17:00:49 +0700

    Add project files.

---
+++ Helicopter Shooting/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Helicopter_Shooting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pillar2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pillar1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pillar2
+            // 
+            this.pillar2.BackColor = System.Drawing.Color.Transparent;
+            this.pillar2.Image = global::Helicopter_Shooting.Properties.Resources.pillar;
+            this.pillar2.Location = new System.Drawing.Point(244, 270);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(47, 181);
+            this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar2.TabIndex = 1;
+            this.pillar2.TabStop = false;
+            this.pillar2.Tag = "pillar";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Helicopter_Shooting.Properties.Resources.Halicopter;
+            this.player.Location = new System.Drawing.Point(109, 126);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 50);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // ufo
+            // 
+            this.ufo.BackColor = System.Drawing.Color.Transparent;
+            this.ufo.Image = global::Helicopter_Shooting.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(551, 253);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(63, 60);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ufo.TabIndex = 3;
+            this.ufo.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.BackColor = System.Drawing.Color.Khaki;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(13, 13);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(110, 29);
+            this.txtScore.TabIndex = 4;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // pillar1
+            // 
+            this.pillar1.BackColor = System.Drawing.Color.Transparent;
+            this.pillar1.Image = global::Helicopter_Shooting.Properties.Resources.pillar;
+            this.pillar1.Location = new System.Drawing.Point(468, 1);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(47, 164);
+            this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar1.TabIndex = 5;
+            this.pillar1.TabStop = false;
+            this.pillar1.Tag = "pillar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Helicopter_Shooting.Properties.Resources.bg__1_;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pillar1);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pillar2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pillar2;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox pillar1;
+    }
+}
+
---
+++ Helicopter Shooting/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Helicopter Shooting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helicopter Shooting")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c0795a1-7bc6-492d-8e12-9ed2f89143fc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
